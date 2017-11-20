b47ecb5fd0c7325b0640c0fd9354b422ac9a48ad
Andrew McCullough <andrewmccullough@users.noreply.github.com>
2017-11-20 15:46:51 -0500

    Big changes. Huuuge.

--- game.py
+++ game.py
@@ -7,46 +7,54 @@ import random
 # Initialize camera
 camera = gamebox.Camera(800, 600)
 
-# Create ball
-ball = gamebox.from_image(0, 0, "assets/moon.png")
-
-# Globals
-start_game = False
+# Global variables
 counter = 0
 score = 0
 stars = []
+blocks = []
 
-def background():
-    '''Creates a scrolling starry background'''
-    global counter
-    counter += 1
+# def background():
+#
+#     '''
+#         Creates a scrolling starry background
+#     '''
+#
+#     camera.clear('black')
+#
+#
+#
+#     # # Scrolling background
+#     # if counter % 10 == 0:
+#     #     num_stars = random.randint(5, 10)
+#     #     for _ in range(num_stars):
+#     #         stars.append(
+#     #             gamebox.from_color(random.randint(0, 800), 0, 'white', 2, 2))
+#     #
+#     # for star in stars:
+#     #     star.y += 1
+#     #     if star.y > 600:
+#     #         stars.remove(star)
+#     #     camera.draw(star)
 
-    # Initialize background
-    if start_game:
-        for i in range(0, 600, 5):
-            stars.append(
-                gamebox.from_color(random.randint(0, 800), i, 'white', 2, 2))
+# Create ball sprite
+ball = gamebox.from_image(0, 0, "assets/moon.png")
+# Create bouncy platform
+platform = gamebox.from_color(400, 600, "yellow", 150, 40)
 
-        for star in stars:
-            camera.draw(star)
-
-    # Scrolling background
-    if counter % 10 == 0:
-        num_stars = random.randint(5, 10)
-        for _ in range(num_stars):
-            stars.append(
-                gamebox.from_color(random.randint(0, 800), 0, 'white', 2, 2))
-
-    for star in stars:
-        star.y += 1
-        if star.y > 600:
-            stars.remove(star)
-        camera.draw(star)
-
+# Generates a fuckton of stars
+while len(stars) < 200:
+    stars.append(
+        gamebox.from_color(
+            random.randint(0, 800),
+            random.randint(0, 600),
+            "white",
+            2,
+            2
+        )
+    )
 
 def tick(keys):
-    '''Game loop'''
-    global start_game
+
     global score
 
     # Start the game
@@ -58,24 +66,13 @@ def tick(keys):
     #             'Andrew McCullough (asm4wm) and Samarth Kishor (sk4gz)',
     #             'Arial', 50, 'red'))
 
-    # Controls
-    if pygame.K_UP in keys:
-        character.y -= 5
-    if pygame.K_DOWN in keys:
-        character.y += 5
-    if pygame.K_LEFT in keys:
-        character.x -= 5
-    if pygame.K_RIGHT in keys:
-        character.x += 5
-
-    # Background
     camera.clear('black')
-    background()
 
-    # Draw
-    camera.draw(character)
+    for star in stars:
+        camera.draw(star)
+    camera.draw(ball)
+    camera.draw(platform)
 
     camera.display()
-
 
 gamebox.timer_loop(30, tick)
