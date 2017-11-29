d240ddaa2116858a2a02fd0286508ba1fc13ba88
Andrew McCullough <andrewmccullough@users.noreply.github.com>
2017-11-29 16:13:37 -0500

    Better game over.

--- game.py
+++ game.py
@@ -14,6 +14,7 @@ aliens = []
 colors = ['red', 'orange', 'green', 'blue', 'purple']
 alien_sprites = ["alien_blue.png", "alien_green.png", "alien_pink.png", "alien_purple.png"]
 game_started = False
+game_over = False
 ball_speed = 10
 ball_angle = 0
 
@@ -38,6 +39,11 @@ def vectorize(angle):
 
 
 def endgame(won, score):
+    global ball
+    global platform
+
+    ball = gamebox.from_color(0, 0, "black", 0, 0)
+
     camera.clear('black')
     if won:
         scoreboard = gamebox.from_text(
@@ -56,104 +62,110 @@ def tick(keys):
     global game_started
     global score
     global counter
+    global game_over
 
     counter += 1
 
-    if counter % (45 * 3) == 0:
-        # generate alien
-        new_alien = random.choice(alien_sprites) # picks a random alien
-        aliens.append(gamebox.from_image(random.randint(100, 700), 0, new_alien))
+    if not game_over:
 
-    if pygame.K_LEFT in keys:
-        # moves platform left
-        platform.x -= 12
+        if counter % (45 * 3) == 0:
+            # generate alien
+            new_alien = random.choice(alien_sprites) # picks a random alien
+            aliens.append(gamebox.from_image(random.randint(100, 700), 0, new_alien))
 
-        if not game_started:
-            # releases the ball from the platform
-            ball.xspeed, ball.yspeed = vectorize(110)
-            game_started = True
+        if pygame.K_LEFT in keys:
+            # moves platform left
+            platform.x -= 12
 
-    if pygame.K_RIGHT in keys:
-        # moves platform right
-        platform.x += 12
+            if not game_started:
+                # releases the ball from the platform
+                ball.xspeed, ball.yspeed = vectorize(110)
+                game_started = True
 
-        if not game_started:
-            # releases the ball from the platform
-            ball.xspeed, ball.yspeed = vectorize(70)
-            game_started = True
+        if pygame.K_RIGHT in keys:
+            # moves platform right
+            platform.x += 12
 
-    # Correct overshooting
-    if platform.x > 800 - 160 / 2:
-        # platform has overshot to the right
-        platform.x = 800 - 160 / 2
-    if platform.x < 160 / 2:
-        # platform has overshot to the left
-        platform.x = 160 / 2
+            if not game_started:
+                # releases the ball from the platform
+                ball.xspeed, ball.yspeed = vectorize(70)
+                game_started = True
 
-    # Implement ball speed
-    ball.x = ball.x + ball.xspeed
-    ball.y = ball.y - ball.yspeed
+        # Correct overshooting
+        if platform.x > 800 - 160 / 2:
+            # platform has overshot to the right
+            platform.x = 800 - 160 / 2
+        if platform.x < 160 / 2:
+            # platform has overshot to the left
+            platform.x = 160 / 2
 
-    # Draw the background within the game loop
-    camera.clear('black')
+        # Implement ball speed
+        ball.x = ball.x + ball.xspeed
+        ball.y = ball.y - ball.yspeed
 
-    for star in stars:
-        camera.draw(star)
+        # Draw the background within the game loop
+        camera.clear('black')
 
-    # Collision detection
-    for block in blocks:
-        if ball.touches(block):
-            if ball_angle > 90 and ball_angle < 180:
-                ball.xspeed, ball.yspeed = vectorize(180 + (180 - ball_angle))
-            elif ball_angle > 0 and ball_angle < 90:
-                ball.xspeed, ball.yspeed = vectorize(0 - ball_angle)
+        for star in stars:
+            camera.draw(star)
 
-            blocks.remove(block)
-            score += 1
+        # Collision detection
+        for block in blocks:
+            if ball.touches(block):
+                if ball_angle > 90 and ball_angle < 180:
+                    ball.xspeed, ball.yspeed = vectorize(180 + (180 - ball_angle))
+                elif ball_angle > 0 and ball_angle < 90:
+                    ball.xspeed, ball.yspeed = vectorize(0 - ball_angle)
 
-        camera.draw(block)
+                blocks.remove(block)
+                score += 1
 
-    if ball.touches(platform):
-        if ball.x < platform.x:
-            ball.xspeed, ball.yspeed = vectorize(130 +
-                                                 (platform.x - ball.x) / 8)
-        elif ball.x > platform.x:
-            ball.xspeed, ball.yspeed = vectorize(50 -
-                                                 (ball.x - platform.x) / 8)
+            camera.draw(block)
 
-    # Draw alien_sprites
+        if ball.touches(platform):
+            if ball.x < platform.x:
+                ball.xspeed, ball.yspeed = vectorize(130 +
+                                                     (platform.x - ball.x) / 8)
+            elif ball.x > platform.x:
+                ball.xspeed, ball.yspeed = vectorize(50 -
+                                                     (ball.x - platform.x) / 8)
 
-    for alien in aliens:
-        alien.y += 4
+        # Draw alien_sprites
 
-        if ball.touches(alien):
-            score += 1
-            aliens.remove(alien)
+        for alien in aliens:
+            alien.y += 2
 
-        if platform.touches(alien):
+            if ball.touches(alien):
+                score += 1
+                aliens.remove(alien)
+
+            if platform.touches(alien):
+                endgame(False, score)
+                game_over = True
+
+            camera.draw(alien)
+
+        # Bounce the ball off the sides of the screen
+        if ball.y > 600 - 54 / 2:
             endgame(False, score)
+            game_over = True
 
-        camera.draw(alien)
+        if ball.y < 0 + 54 / 2:
+            if 0 < ball_angle < 90:
+                ball.xspeed, ball.yspeed = vectorize(180 - (ball_angle - 180))
+            elif 90 < ball_angle < 180:
+                ball.xspeed, ball.yspeed = vectorize(360 - ball_angle)
 
-    # Bounce the ball off the sides of the screen
-    if ball.y > 600 - 54 / 2:
-        endgame(False, score)
+        if ball.x < 0 + 54 / 2:
+            ball.xspeed, ball.yspeed = vectorize(90 - (ball_angle - 90))
 
-    if ball.y < 0 + 54 / 2:
-        if 0 < ball_angle < 90:
-            ball.xspeed, ball.yspeed = vectorize(180 - (ball_angle - 180))
-        elif 90 < ball_angle < 180:
-            ball.xspeed, ball.yspeed = vectorize(360 - ball_angle)
+        if ball.x > 800 - 54 / 2:
+            ball.xspeed, ball.yspeed = vectorize(90 + (90 - ball_angle))
 
-    if ball.x < 0 + 54 / 2:
-        ball.xspeed, ball.yspeed = vectorize(90 - (ball_angle - 90))
-
-    if ball.x > 800 - 54 / 2:
-        ball.xspeed, ball.yspeed = vectorize(90 + (90 - ball_angle))
-
-    # End the game
-    if len(blocks) == 0:
-        endgame(True, score)
+        # End the game
+        if len(blocks) == 0:
+            endgame(True, score)
+            game_over = True
 
     if pygame.K_q in keys:
         gamebox.stop_loop()
